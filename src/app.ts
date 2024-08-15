f6e0738c37e56f0b4343e1e5276f4358b208e60b
Mohammad Ibrahim <mdiibrahim549@gmail.com>
2024-08-15 16:59:48 +0600

    readme.md and license are added

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,22 @@ import notFoundRoute from './app/middlewares/notFoundRoute';
 import router from './app/routes';
 const app: Application = express();
 
+//parsers
 app.use(express.json());
 app.use(cors());
 
+//routes
 app.use('/api', router);
 
+//centralized error handler
 app.use(globalErrorHandler);
-app.use(notFoundRoute);
 
+//root route
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello Sports-Facility-Booking-Platform!');
 });
+
+//not found route
+app.use(notFoundRoute);
 
 export default app;
